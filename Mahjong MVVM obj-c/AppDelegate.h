ab4fc03f77022353d5a44a217b3ef7877b1549b5
Tsvihun Oleksandr <mircamnia@ukr.net>
2025-02-21 19:55:16 +0100

    Initial Commit

---
+++ Mahjong MVVM obj-c/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Mahjong MVVM obj-c
+//
+//  Created by Aleksandr Tsvihun on 21.02.2025.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Mahjong MVVM obj-c/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Mahjong MVVM obj-c
+//
+//  Created by Aleksandr Tsvihun on 21.02.2025.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
